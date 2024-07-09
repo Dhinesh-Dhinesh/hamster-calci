237ed09097b1c8a2efb89deaf7addc215b2b7427
Dhinesh-Dhinesh <dhineshrandy953@gmail.com>
2024-07-09 17:58:26 +0530

    added port

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
     res.send("Bot is alive")
 })
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`server is running on ${port}`)
